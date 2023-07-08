7fc10892c2f1a32bb11dde736dec61e093f49b9f
adrict99 <adrian.castro@digio.es>
2023-07-08 15:20:32 +0200

    Add more methods to UserRepository and WorkoutRepository

--- src/main/kotlin/com/adrict99/atlas_app_backend/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/adrict99/atlas_app_backend/domain/repository/UserRepository.kt
@@ -1,19 +1,23 @@
 package com.adrict99.atlas_app_backend.domain.repository
 
+import com.adrict99.atlas_app_backend.domain.entity.Routine
 import com.adrict99.atlas_app_backend.domain.entity.User
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface UserRepository {
 
     fun saveUser(user: User): User
 
-    fun findUserById(userId: Long): User
+    fun findUserById(id: Long): User
 
-    fun findUserByEmail(userEmail: String): User
+    fun findUserByEmail(email: String): User
 
     fun updateUser(user: User): User
+
+    fun updateUserNameById(id: Long, name: String): User
+
+    fun updateUserRoutinesById(id: Long, routines: List<Routine>): User
 
     fun deleteUserById(id: Long): Boolean
 
--- src/main/kotlin/com/adrict99/atlas_app_backend/domain/repository/WorkoutRepository.kt
+++ src/main/kotlin/com/adrict99/atlas_app_backend/domain/repository/WorkoutRepository.kt
@@ -1,5 +1,6 @@
 package com.adrict99.atlas_app_backend.domain.repository
 
+import com.adrict99.atlas_app_backend.domain.entity.Exercise
 import com.adrict99.atlas_app_backend.domain.entity.Workout
 import org.springframework.stereotype.Repository
 
@@ -13,6 +14,12 @@ interface WorkoutRepository {
     fun findWorkoutById(workoutId: Long): Workout
 
     fun updateWorkout(workout: Workout): Workout
+
+    fun updateWorkoutNameById(workoutId: Long, workoutName: String): Workout
+
+    fun updateWorkoutExercisesById(workoutId: Long, newExercises: List<Exercise>): Workout
+
+    fun updateWorkoutNotesById(workoutId: Long, newNotes: String?): Workout
 
     fun deleteWorkoutById(workoutId: Long): Boolean
 
